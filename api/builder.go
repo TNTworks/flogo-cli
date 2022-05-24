fd28055b21523d8fdc2dbf5066a39846ab34de06
Kristián Gerencséri <kristian.gerencseri@o2.sk>
2022-05-24 22:02:51 +0200

    fix

--- api/builder.go
+++ api/builder.go
@@ -44,7 +44,13 @@ func simpleGoBuild(project common.AppProject) error {
 		fmt.Println("Performing 'go build'...")
 	}
 
-	err := util.ExecCmd(exec.Command("go", "build", "-o", project.Executable()), project.SrcDir())
+	err := util.ExecCmd(exec.Command("go", "mod", "tidy"), project.SrcDir())
+	if err != nil {
+		fmt.Println("Error in building", project.SrcDir())
+		return err
+	}
+
+	err = util.ExecCmd(exec.Command("go", "build", "-o", project.Executable()), project.SrcDir())
 	if err != nil {
 		fmt.Println("Error in building", project.SrcDir())
 		return err
